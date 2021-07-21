70ec8d6873fed2f8a9ac7d078db29e3093716b6d
Alex Marriott <marriottaog@gmail.com>
2021-07-21 15:26:55 +0100

    youtube API now allows searching for video ideas using a given search term

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,13 @@ class PagesController < ApplicationController
     url = "https://rhymebrain.com/talk?function=getRhymes&word=#{@rand_word}"
     rhyming_words_array = JSON.parse(URI.open(url).read).shuffle[0...100]
     @rhyming_words = rhyming_words_array.map {|rhyme_hash| rhyme_hash["word"] }
+
+    url2 = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=trap_freestyle&maxResults=50&type=video&videoDuration=short&key=AIzaSyCMOG5RQYDzlOJd-blRPdYZi6uVxLoHCoY"
+
+    video_ids = JSON.parse(URI.open(url2).read)["items"].map do |item|
+      item["id"]["videoId"]
+    end
+
+    @random_video_id = video_ids.sample
   end
 end
