b67603f207289bfc6fceaf4b9a6847b51b89ea53
Alex Marriott <marriottaog@gmail.com>
2021-07-21 11:02:11 +0100

    page now displays 100 random rhyming words in basic format

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    # url = 'https://random-word-api.herokuapp.com/word?number=1'
-    # word_serialized = URI.open(url).read
-    # word = JSON.parse(word_serialized)[0]
-
-    # url2 = "https://api.datamuse.com/words?rel_nry=#{word}"
-    # thing = URI.open(url).read
-    # ahh = JSON.parse(thing)
-
-    raise
+    @rand_word = Word.all.sample.name
+    url = "https://rhymebrain.com/talk?function=getRhymes&word=#{@rand_word}"
+    rhyming_words_array = JSON.parse(URI.open(url).read).shuffle[0...100]
+    @rhyming_words = rhyming_words_array.map {|rhyme_hash| rhyme_hash["word"] }
   end
 end
