ebbfe98d3299fcb8113f189b4405a5e7b80f8bb4
Alex Marriott <marriottaog@gmail.com>
2021-07-21 10:43:58 +0100

    added word model, basic calls to rhyme API and started progress on writing txt file from seed to word model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
+  require 'json'
+  require 'open-uri'
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # url = 'https://random-word-api.herokuapp.com/word?number=1'
+    # word_serialized = URI.open(url).read
+    # word = JSON.parse(word_serialized)[0]
+
+    # url2 = "https://api.datamuse.com/words?rel_nry=#{word}"
+    # thing = URI.open(url).read
+    # ahh = JSON.parse(thing)
+
+    raise
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p 'hey'
+
+words = File.open("db/words.txt").read.split
