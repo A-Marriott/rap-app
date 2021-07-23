05a95f85c1e9467eb3e4167dd6670896c1f0b436
Alex Marriott <marriottaog@gmail.com>
2021-07-23 14:58:28 +0100

    basic formatting for page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @rand_word = Word.all.sample.name
+    @rand_word = Word.all.sample.name.capitalize
     url = "https://rhymebrain.com/talk?function=getRhymes&word=#{@rand_word}"
-    rhyming_words_array = JSON.parse(URI.open(url).read).shuffle[0...100]
-    @rhyming_words = rhyming_words_array.map {|rhyme_hash| rhyme_hash["word"] }
+    rhyming_words_array = JSON.parse(URI.open(url).read)[0...100]
+    @rhyming_words = rhyming_words_array.map {|rhyme_hash| rhyme_hash["word"].capitalize }
 
-    url2 = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=boombap_freestyle&maxResults=50&type=video&videoDuration=short&key="
+    url2 = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=boombap_freestyle&maxResults=50&type=video&videoDuration=short&key=AIzaSyCBPnljDFXcHYd4v5-PklyJaD-VHzWNRSE"
 
     video_ids = JSON.parse(URI.open(url2).read)["items"].map do |item|
       item["id"]["videoId"]
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,5 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+
 });
