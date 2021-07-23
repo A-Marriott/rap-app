a479c6aa353efeaf73789f8ad604d40bee1147b6
Alex Marriott <marriottaog@gmail.com>
2021-07-23 16:01:43 +0100

    added api key to env

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   require 'json'
   require 'open-uri'
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @rand_word = Word.all.sample.name.capitalize
@@ -9,12 +9,14 @@ class PagesController < ApplicationController
     rhyming_words_array = JSON.parse(URI.open(url).read)[0...100]
     @rhyming_words = rhyming_words_array.map {|rhyme_hash| rhyme_hash["word"].capitalize }
 
-    url2 = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=boombap_freestyle&maxResults=50&type=video&videoDuration=short&key=AIzaSyCBPnljDFXcHYd4v5-PklyJaD-VHzWNRSE"
+    url2 = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=boombap_freestyle&maxResults=50&type=video&videoDuration=short&key=#{ENV['YOUTUBE_API_KEY']}"
 
     video_ids = JSON.parse(URI.open(url2).read)["items"].map do |item|
       item["id"]["videoId"]
     end
 
     @random_video_id = video_ids.sample
+
+    # raise
   end
 end
