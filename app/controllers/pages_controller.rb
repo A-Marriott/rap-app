aa0b2b310977de0ff1908d2bb87c97dfc8e6f6b9
Alex Marriott <marriottaog@gmail.com>
2021-07-26 13:53:45 +0100

    added basic links to allow choosing of genre, need to fix bug where first load is ineffective

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,18 +5,36 @@ class PagesController < ApplicationController
 
   def home
     @rand_word = Word.all.sample.name.capitalize
-    url = "https://rhymebrain.com/talk?function=getRhymes&word=#{@rand_word}"
-    rhyming_words_array = JSON.parse(URI.open(url).read)[0...100]
+    rhyming_word_url = "https://rhymebrain.com/talk?function=getRhymes&word=#{@rand_word}"
+    rhyming_words_array = JSON.parse(URI.open(rhyming_word_url).read)[0...100]
     @rhyming_words = rhyming_words_array.map {|rhyme_hash| rhyme_hash["word"].capitalize }
 
-    url2 = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=boombap_freestyle&maxResults=50&type=video&videoDuration=short&key=#{ENV['YOUTUBE_API_KEY']}"
+    search_string = 'freestyle_instrumental'
 
-    video_ids = JSON.parse(URI.open(url2).read)["items"].map do |item|
+    case params["q"]
+    when 'boombap'
+      search_string = 'boombap_instrumental'
+    when 'trap'
+      search_string = 'trap_instrumental'
+    when 'drill'
+      search_string = 'drill_instrumental'
+    when 'lofi'
+      search_string = 'lofi_instrumental'
+    when 'grime'
+      search_string = 'grime_instrumental'
+    when 'jazz_rap'
+      search_string = 'jazz_rap_instrumental'
+    end
+
+    youtube_video_search_url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=#{search_string}&maxResults=50&type=video&videoDuration=short&key=#{ENV['YOUTUBE_API_KEY']}"
+
+    video_ids = JSON.parse(URI.open(youtube_video_search_url).read)["items"].map do |item|
       item["id"]["videoId"]
     end
 
     @random_video_id = video_ids.sample
 
+# http://localhost:3000/posts/?query=M3
     # raise
   end
 end
