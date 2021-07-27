bc9df63b9c1178034fd229561271228059f7efdd
Alex Marriott <marriottaog@gmail.com>
2021-07-27 14:35:29 +0100

    controller now pulls a video ID from the database, and the page refresh bug is fixed using iframe rather than youtube api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,33 +7,26 @@ class PagesController < ApplicationController
     @rand_word = Word.all.sample.name.capitalize
     rhyming_word_url = "https://rhymebrain.com/talk?function=getRhymes&word=#{@rand_word}"
     rhyming_words_array = JSON.parse(URI.open(rhyming_word_url).read)[0...100]
-    @rhyming_words = rhyming_words_array.map {|rhyme_hash| rhyme_hash["word"].capitalize }
+    @rhyming_words = rhyming_words_array.map { |rhyme_hash| rhyme_hash["word"].capitalize }
 
     search_string = case params["q"]
                     when 'boombap'
-                      'boombap_instrumental'
+                      'boombap'
                     when 'trap'
-                      'trap_instrumental'
+                      'trap'
                     when 'drill'
-                      'drill_instrumental'
+                      'drill'
                     when 'lofi'
-                      'lofi_instrumental'
+                      'lofi'
                     when 'grime'
-                      'grime_instrumental'
+                      'grime'
                     when 'jazz_rap'
-                      'jazz_rap_instrumental'
+                      'jazz_rap'
                     else
-                      'freestyle_instrumental'
+                      'freestyle'
                     end
 
-    youtube_video_search_url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=#{search_string}&maxResults=50&type=video&videoDuration=short&key=#{ENV['YOUTUBE_API_KEY']}"
-
-    video_ids = JSON.parse(URI.open(youtube_video_search_url).read)["items"].map do |item|
-      item["id"]["videoId"]
-    end
-
-    @random_video_id = video_ids.sample
-
+    @random_video_id = YoutubeVideo.where(genre: search_string).order(Arel.sql('RANDOM()')).first.video_id
     # raise
   end
 end
